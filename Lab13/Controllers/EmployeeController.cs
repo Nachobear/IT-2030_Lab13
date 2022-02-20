d2d4c9aec59447c69ff9f6eaac51f0bd34df604d
Nachobear <kainerob@gmail.com>
2022-02-20 01:10:12 -0500

    Add project files.

---
+++ Lab13/Controllers/EmployeeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab13.Models;
+using Lab13.Models.Validation;
+
+namespace Lab13.Controllers
+{
+    public class EmployeeController : Controller
+    {
+       
+         private SalesContext context { get; set; }
+
+        public EmployeeController(SalesContext ctx) => context = ctx;
+
+        public IActionResult Index() => RedirectToAction("Index", "Name");
+        
+        [HttpGet]
+        public ViewResult Add()
+        {
+            ViewBag.Employees = context.Employees.OrderBy(e => e.LastName).ThenBy(e => e.FirstName).ToList();
+            return View();
+        }
+
+        public IActionResult Add(Employee employee)
+        {
+            string message = Validate.CheckEmployee(context, employee);
+            if (!string.IsNullOrEmpty(message))
+            {
+                ModelState.AddModelError(nameof(Employee.DateOfBirth), message);
+            }
+
+            message = Validate.CheckManagerEmployeeMatch(context, employee);
+            if (!string.IsNullOrEmpty(message))
+            {
+                ModelState.AddModelError(nameof(Employee.ManagerId), message);
+            }
+
+            if (ModelState.IsValid)
+            {
+                context.Employees.Add(employee);
+                context.SaveChanges();
+                TempData["message"] = $"Employee {employee.FullName} added";
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Employees = context.Employees.OrderBy(e => e.LastName).ThenBy(e => e.FirstName).ToList();
+                return View();
+            }
+        }
+        
+    }
+}
---
+++ Lab13/Controllers/SalesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab13.Models;
+using Lab13.Models.Validation;
+
+
+namespace Lab13.Controllers
+{
+    public class SalesController : Controller
+    {
+
+
+        private SalesContext context { get; set; }
+
+        public SalesController(SalesContext ctx) => context = ctx;
+
+        public IActionResult Index() => RedirectToAction("Index", "Home");
+
+        [HttpGet]
+        public ViewResult Add()
+        {
+            ViewBag.Employees = context.Employees.OrderBy(e => e.LastName).ThenBy(e => e.FirstName).ToList();
+            return View();
+        }
+
+        public IActionResult Add(Sales sales)
+        {
+            string message = Validate.CheckSales(context, sales);
+            if (!string.IsNullOrEmpty(message))
+            {
+                ModelState.AddModelError(nameof(sales.EmployeeId), message);
+            }
+
+            if (ModelState.IsValid)
+            {
+                context.Sales.Add(sales);
+                context.SaveChanges();
+                TempData["message"] = $"Sales added";
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Employees = context.Employees.OrderBy(e => e.LastName).ThenBy(e => e.FirstName).ToList();
+                return View();
+            }
+        }
+
+
+    }
+}
